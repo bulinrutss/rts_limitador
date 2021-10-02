735fec216e39960089d85494dd60dd926999f195
bulinrutss <77656522+bulinrutss@users.noreply.github.com>
2021-10-02 17:45:26 -0300

    Add files via upload

---
+++ rts_limitador/config.lua
@@ -0,0 +1,8 @@
+--LIMITADOR CON MARCADOR EN EL MAPA DONDE TE OBLIGA A BAJAR LA VELOCIDAD
+Config = {}
+Config.zones = {
+	{ ['x'] = -299.8, ['y'] = -938.47, ['z'] = 31.08 }
+
+}
+Config.radius = 75.0 -- RADIO DEL LIMITADOR
+Config.speedlimitador = 20.0 -- VELOCIDAD QUE SE REDUCIRA CUANDO ESTES PASES POR ESA ZONA EN (KPH)
